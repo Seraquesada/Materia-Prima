ad5234429092974a01e03d58e8e8cbc85ac55b96
bautista00 <quesada.serafin03@gmail.com>
2024-07-27 18:56:40 -0300

    modal componet

---
+++ src/components/Modal.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image"
+
+const Modal = () => {
+	return (
+		<div>
+			<Image
+				className=""
+				width={350}
+				height={350}
+				src={'/static/img/blanca.webp'}
+				alt={'aaa'}
+			/>
+		</div>
+	)
+}
+
+export default Modal
