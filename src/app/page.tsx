3c37f5cbf296f7b921a25707a321fd37c72b0239
bautista00 <quesada.serafin03@gmail.com>
2024-07-27 18:55:55 -0300

    improving main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
+import Main from '@/components/Main'
 import Navbar from '@/components/Navbar'
+import Tecnicas from '@/components/Tecnicas'
 
 export default function Home() {
 	return (
-		<main>
+		<main className="flex flex-col items-center bg-gradient-to-b from-neutral-950 from-30% to-[#751006] text-gray-200">
 			<Navbar />
+			<div className="w-10/12">
+				<Main />
+				<Tecnicas />
+			</div>
 		</main>
 	)
 }
