88fa7a47843ea08dfde0022391a4b68ff2f215d8
bautista00 <quesada.serafin03@gmail.com>
2024-07-29 17:38:09 -0300

    adding navbar clientes and smooth behavior

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,9 @@
 'use client'
 import Link from 'next/link'
-import { usePathname } from 'next/navigation'
 
 const links = [
 	{
-		href: '/',
+		href: '/#inicio',
 		text: 'Inicio',
 	},
 	{
@@ -16,25 +15,30 @@ const links = [
 		text: 'Sobre Nosotros',
 	},
 	{
+		href: '/#clientes',
+		text: 'Clientes',
+	},
+	{
 		href: '/#contacto',
 		text: 'Contacto',
 	},
 ]
 
 const Navbar = () => {
-	const pathname = usePathname()
+
 	return (
 		<nav className="sticky top-0 flex w-full justify-between bg-neutral-950 px-32 py-4 text-gray-200">
 			<h4 className="font-bold"> MATERIA PRIMA</h4>
 			<ul className="flex gap-4">
 				{links.map((l) => (
-					<Link
-						key={l.text}
-						className={'border-b border-[#D9D9D9] px-5 text-xl'}
-						href={l.href}
-					>
-						{l.text}
-					</Link>
+					<li key={l.text}>
+						<Link
+							className="scroll-smooth border-b border-[#D9D9D9] px-5 text-xl"
+							href={l.href}
+						>
+							{l.text}
+						</Link>
+					</li>
 				))}
 			</ul>
 		</nav>
