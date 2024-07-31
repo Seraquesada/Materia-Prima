90f0b448c0a797d02dc25cf98d112cbc80bef34a
bautista00 <quesada.serafin03@gmail.com>
2024-07-31 16:32:06 -0300

    adding hamburger

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,7 @@
 'use client'
 import Link from 'next/link'
+import Hamburguer from './Hamburguer'
+import { useState } from 'react'
 
 const links = [
 	{
@@ -25,16 +27,31 @@ const links = [
 ]
 
 const Navbar = () => {
+	const [open, setOpen] = useState(false)
+
+	const toggleOpen = () => {
+		setOpen(!open)
+	}
+
+	
 
 	return (
-		<nav className="sticky top-0 flex w-full justify-between bg-neutral-950 px-32 py-4 text-gray-200">
-			<h4 className="font-bold"> MATERIA PRIMA</h4>
-			<ul className="flex gap-4">
+		<nav className="sticky top-0 flex w-full justify-between bg-neutral-950 px-8 py-4 text-gray-200">
+			<h4 className="font-bold">MATERIA PRIMA</h4>
+			<div className="flex items-center md:hidden" onClick={toggleOpen}>
+				<Hamburguer isOpen={open} />
+			</div>
+			<ul
+				className={`fixed left-0 top-0 z-10 h-screen w-48 transform bg-neutral-950 p-8 transition-transform duration-300 ease-in-out md:relative md:left-auto md:top-auto md:flex md:h-auto md:w-auto md:transform-none md:p-0 md:text-center ${
+					open ? 'translate-x-0' : '-translate-x-full'
+				}`}
+			>
 				{links.map((l) => (
-					<li key={l.text}>
+					<li key={l.text} className="my-6 md:my-0 md:ml-4">
 						<Link
-							className="scroll-smooth border-b border-[#D9D9D9] px-5 text-xl"
+							className="scroll-smooth border-b border-[#D9D9D9] px-5 text-xl md:border-none"
 							href={l.href}
+							onClick={() => setOpen(false)}
 						>
 							{l.text}
 						</Link>
