db959c28e4dbb8d2fcfab3be8d3fe0d7f903ba93
bautista00 <quesada.serafin03@gmail.com>
2024-07-31 16:32:40 -0300

    fixing ui

--- src/components/Tecnicas.tsx
+++ src/components/Tecnicas.tsx
@@ -2,46 +2,46 @@ import Image from 'next/image'
 
 const Tecnicas = () => {
 	return (
-		<section className="h-screen" id="tecnicas">
+		<section className="" id="tecnicas">
 			<div className="flex flex-col justify-center gap-16 pt-24 text-center">
 				<h2 className="text-7xl font-semibold">Tecnicas</h2>
-				<section className="grid grid-cols-1 place-items-center gap-5 md:grid-cols-3 lg:grid-cols-5">
+				<section className="grid grid-cols-1 place-items-center gap-5 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
 					{' '}
 					<Image
 						className="cursor-pointer"
 						width={350}
 						height={350}
-						src={'/static/img/blanca.webp'}
+						src={'/img/blanca.webp'}
 						alt={'aaa'}
 					/>{' '}
 					<Image
-						className=""
+						className="cursor-pointer"
 						width={350}
 						height={350}
-						src={'/static/img/blanca.webp'}
+						src={'/img/blanca.webp'}
 						alt={'aaa'}
 					/>{' '}
 					<Image
-						className=""
+						className="cursor-pointer"
 						width={350}
 						height={350}
-						src={'/static/img/blanca.webp'}
+						src={'/img/blanca.webp'}
 						alt={'aaa'}
 					/>{' '}
 					<Image
-						className=""
+						className="cursor-pointer"
 						width={350}
 						height={350}
-						src={'/static/img/blanca.webp'}
+						src={'/img/blanca.webp'}
 						alt={'aaa'}
 					/>{' '}
 					<Image
-						className=""
+						className="cursor-pointer"
 						width={350}
 						height={350}
-						src={'/static/img/blanca.webp'}
+						src={'/img/blanca.webp'}
 						alt={'aaa'}
-					/>
+					/>{' '}
 				</section>
 			</div>
 		</section>
