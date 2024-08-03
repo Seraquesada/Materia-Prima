20565b39075172f9344f5164f2e4a56807ce4791
bautista00 <quesada.serafin03@gmail.com>
2024-08-03 15:14:45 -0300

    adding  clientes

--- src/components/Clientes.tsx
+++ src/components/Clientes.tsx
@@ -5,18 +5,20 @@ import puma from '../../public/puma.png'
 import herencia from '../../public/herencia.png'
 import vans from '../../public/vans.png'
 import kosiuko from '../../public/kosiuko.png'
-
+import cheeky from '../../public/cheeky.png'
+import mimo from '../../public/mimo.png'
+import grisino from '../../public/grisino.png'
 
 const Clientes = () => {
 	return (
 		<section className="" id="clientes">
 			<div className="flex flex-col items-center justify-center gap-10 pt-24 text-center">
-				<h2 className="text-7xl font-semibold">Clientes</h2>
+				<h2 className="text-5xl font-semibold lg:text-7xl">Clientes</h2>
 				<h4 className="text-3xl font-semibold">
 					Empresas que ya confiaron en nosotros
 				</h4>
 
-				<div className="flex flex-wrap justify-center gap-16">
+				<div className="flex flex-wrap justify-center gap-20">
 					<Image
 						src={adidas}
 						alt="adidas"
@@ -46,12 +48,27 @@ const Clientes = () => {
 						src={kosiuko}
 						alt="kosiuko"
 						className="w-32 object-contain sm:w-40 md:w-48"
+					/>{' '}
+					<Image
+						src={mimo}
+						alt="mimo"
+						className="w-32 object-contain sm:w-40 md:w-48"
+					/>{' '}
+					<Image
+						src={grisino}
+						alt="grisino"
+						className="w-32 object-contain sm:w-40 md:w-48"
+					/>{' '}
+					<Image
+						src={cheeky}
+						alt="cheeky"
+						className="w-32 object-contain sm:w-40 md:w-48"
 					/>
 				</div>
 
 				<div>
-					<h4 className="text-3xl font-semibold">
-						
+					<h4 className="pt-10 text-3xl font-semibold">
+						Dejemos que ellos hablen por nosotros
 					</h4>
 				</div>
 			</div>
