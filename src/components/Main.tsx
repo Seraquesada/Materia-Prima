0b359f5f3bd442c89a9d5cec2e9bab6d31d1830d
bautista00 <quesada.serafin03@gmail.com>
2024-08-05 19:40:04 -0300

    basci animation for title

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,19 +1,13 @@
 import Slider from './Slider'
+import Title from './Title'
 
 const Main = () => {
 	return (
 		<section
-			className="flex h-[92.5vh] flex-col items-center justify-center gap-14 md:gap-0 pt-24 md:flex-row md:justify-between md:pt-0"
+			className="flex h-[92.5vh] flex-col items-center justify-center gap-14 pt-24 md:flex-row md:justify-between md:gap-0 md:pt-0"
 			id="inicio"
 		>
-			<div className="text-center md:text-left">
-				<h1 className="text-5xl font-semibold sm:text-6xl lg:text-8xl">
-					MATERIA PRIMA
-				</h1>
-				<h3 className="text-xl md:text-2xl lg:text-3xl">
-					Todo lo que necesitas para estampar
-				</h3>
-			</div>
+			<Title />
 			<Slider />
 		</section>
 	)
---
+++ src/components/Title.tsx
@@ -0,0 +1,30 @@
+'use client'
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
+import { useRef } from 'react'
+
+const Title = () => {
+	const title = useRef(null)
+
+	useGSAP(
+		() => {
+			gsap.from('.title', {
+				opacity: 0,
+				stagger: 0.8,
+			})
+		},
+		{ scope: title },
+	)
+	return (
+		<div ref={title} className="text-center md:text-left">
+			<h1 className="title text-5xl font-semibold sm:text-6xl lg:text-8xl">
+				MATERIA PRIMA
+			</h1>
+			<h3 className="title text-xl md:text-2xl lg:text-3xl">
+				Todo lo que necesitas para estampar
+			</h3>
+		</div>
+	)
+}
+
+export default Title
