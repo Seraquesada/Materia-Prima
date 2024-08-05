6f4a4395aff3629696cf6b0b7cd801227e8f8bce
bautista00 <quesada.serafin03@gmail.com>
2024-08-05 16:33:37 -0300

    improving responsive

--- src/components/SobreNosotros.tsx
+++ src/components/SobreNosotros.tsx
@@ -1,14 +1,13 @@
-import Image from 'next/image'
 import Slider from './Slider'
 
 const SobreNosotros = () => {
 	return (
 		<section className="" id="sobrenosotros">
 			<div className="flex flex-col items-center justify-center gap-24 pt-24 text-center">
-				<h2 className="text-5xl font-semibold lg:text-7xl">
+				<h2 className="text-4xl font-semibold sm:text-5xl lg:text-7xl">
 					Sobre Nosotros
 				</h2>
-				<ul className="grid w-full justify-center gap-20 text-2xl font-semibold sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
+				<ul className="grid w-full justify-center gap-20 text-3xl font-semibold sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
 					<li className="flex flex-col">
 						<span>+80</span>Empleados
 					</li>
@@ -29,9 +28,9 @@ const SobreNosotros = () => {
 						Tecnicas
 					</li>
 				</ul>
-				<div className="flex w-full flex-col justify-evenly items-center text-start md:flex-row">
+				<div className="flex flex-col items-center justify-center gap-8 xl:flex-row">
 					<Slider />
-					<p className="text-3xl">
+					<p className="text-start text-lg sm:text-2xl md:text-3xl lg:w-[65%]">
 						Materia Prima es una empresa con más de 40 años de
 						experiencia en el sector de estampados, especializada en
 						la serigrafía para grandes volúmenes. Nos destacamos por
