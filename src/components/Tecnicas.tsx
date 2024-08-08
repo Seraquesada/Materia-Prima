fd7d6830ca9b0799f830d1f7effe68926ef02f5b
Seraquesada <quesada.serafin@gmail.com>
2024-08-08 15:52:48 -0300

    adding modal

--- src/components/Tecnicas.tsx
+++ src/components/Tecnicas.tsx
@@ -1,6 +1,7 @@
 'use client'
+
 import Image from 'next/image'
-import { useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
@@ -9,75 +10,80 @@ gsap.registerPlugin(ScrollTrigger)
 const imgs = [
 	{
 		src: '/img/blanca.webp',
-		name: 'PLANA',
+		name: 'Plana',
+	},
+	{
+		src: '/img/bordo.webp',
+		name: 'Relieve',
+	},
+	{
+		src: '/img/negra.webp',
+		name: 'Shimmer',
 	},
 	{
 		src: '/img/blanca.webp',
-		name: 'RELIEVE',
+		name: 'Spandex',
 	},
 	{
 		src: '/img/blanca.webp',
-		name: 'SHIMMER',
+		name: 'Corrosion',
 	},
 	{
 		src: '/img/blanca.webp',
-		name: 'SPANDEX',
+		name: 'Plastisol',
 	},
 	{
 		src: '/img/blanca.webp',
-		name: 'CORROSION',
+		name: 'Al agua',
 	},
 	{
 		src: '/img/blanca.webp',
-		name: 'PLASTISOL',
+		name: 'Monocromia',
 	},
 	{
 		src: '/img/blanca.webp',
-		name: 'AL AGUA',
+		name: 'Cuatricromia',
 	},
 	{
 		src: '/img/blanca.webp',
-		name: 'MONOCROMIA',
+		name: 'Fotocromia',
 	},
 	{
 		src: '/img/blanca.webp',
-		name: 'CUATRICROMIA',
+		name: 'Glitter',
 	},
 	{
 		src: '/img/blanca.webp',
-		name: 'FOTOCROMIA',
+		name: 'Flock',
 	},
 	{
 		src: '/img/blanca.webp',
-		name: 'GLITTER',
+		name: 'Foil',
 	},
 	{
 		src: '/img/blanca.webp',
-		name: 'FLOCK',
+		name: 'Caviar',
 	},
 	{
 		src: '/img/blanca.webp',
-		name: 'FOIL',
+		name: 'Transfer',
 	},
 	{
 		src: '/img/blanca.webp',
-		name: 'CAVIAR',
+		name: 'Silicona',
 	},
 	{
 		src: '/img/blanca.webp',
-		name: 'TRANSFER',
-	},
-	{
-		src: '/img/blanca.webp',
-		name: 'SILICONA',
-	},
-	{
-		src: '/img/blanca.webp',
-		name: 'PERLADO ',
+		name: 'Perlado',
 	},
 ]
 
+
 const Tecnicas = () => {
+	const [img, setImg] = useState<string | undefined>()
+	const [name, setName] = useState<string | undefined>()
+	const dialogRef = useRef<HTMLDialogElement | null>(null)
+
 	useGSAP(() => {
 		gsap.from('.imgs', {
 			opacity: 0,
@@ -90,25 +96,90 @@ const Tecnicas = () => {
 		})
 	})
 
+	useEffect(() => {
+		if (!img) return
+		dialogRef.current?.showModal()
+		document.body.style.overflow = 'hidden'
+		dialogRef.current?.addEventListener('close', closeModal)
+		return () => {
+			// eslint-disable-next-line react-hooks/exhaustive-deps
+			dialogRef.current?.removeEventListener('close', closeModal)
+		}
+	}, [img])
+
+	const closeModal = () => {
+		dialogRef.current?.close()
+		setImg(undefined)
+		document.body.style.overflow = ''
+	}
+
+	const onClickImg = (src: string, name: string) => {
+		setImg(src)
+		setName(name)
+	}
+
 	return (
 		<section id="tecnicas" className="tecnicas">
 			<div className="flex flex-col justify-center gap-16 pt-24 text-center">
 				<h2 className="text-4xl font-semibold sm:text-5xl lg:text-7xl">
 					Tecnicas
 				</h2>
-				<section className="grid grid-cols-1 place-items-center gap-5 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
+
+				<dialog ref={dialogRef} className="backdrop:bg-black/45">
+					<div className="z-0max-h-[90vh] relative max-w-[90vw]">
+						<p className="text-center text-2xl font-bold">
+							Tecnica {name}
+						</p>
+						{img && name && (
+							<Image
+								className="w-72 sm:w-96 lg:w-[32rem]"
+								width={450}
+								height={450}
+								src={img}
+								alt={name}
+							/>
+						)}
+					</div>
+					<button
+						onClick={closeModal}
+						className="absolute right-2 top-2 z-10 flex h-5 w-5 items-center rounded-md bg-zinc-400 shadow"
+					>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							strokeWidth={1.5}
+							stroke="black"
+							className="size-7"
+						>
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								d="M6 18L18 6M6 6l12 12"
+							/>
+						</svg>
+						<span className="sr-only"> close</span>
+					</button>
+				</dialog>
+
+				<ul className="grid grid-cols-1 place-items-center gap-5 sm:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5">
 					{' '}
 					{imgs.map((img) => (
-						<Image
-							key={img.name}
-							className="imgs curson-pointer w-64 rounded-md object-contain sm:w-full"
-							width={350}
-							height={350}
-							src={img.src}
-							alt={img.name}
-						/>
+						<li key={img.name}>
+							<button
+								onClick={() => onClickImg(img.src, img.name)}
+							>
+								<Image
+									className="imgs w-64 rounded-md md:w-80 xl:w-[32rem]"
+									width={350}
+									height={350}
+									src={img.src}
+									alt={img.name}
+								/>
+							</button>
+						</li>
 					))}
-				</section>
+				</ul>
 			</div>
 		</section>
 	)
